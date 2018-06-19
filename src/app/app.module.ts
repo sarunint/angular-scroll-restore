d8ca6bb1638a28cef6ab003b69da3d9460304f30
Sarun Intaralawan <sarunint@sarunint.com>
2018-06-20 01:38:16 +0700

    feat: lorem ipsum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
